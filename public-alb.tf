89d2df1188f51f51e16cf458d08734331b11eb59
Srimaan Penugonda <p.srimaan10@gmail.com>
2021-04-16 10:00:37 +0530

    ALB

--- public-alb.tf
+++ public-alb.tf
@@ -36,3 +36,13 @@ tags = {
 Name                  = "allow-public-alb-sg"
 }
 }
+
+resource "aws_lb_listener" "frontend" {
+  load_balancer_arn = aws_lb.public.arn
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = data.terraform_remote_state.frontend.outputs.FRONTEND_TG_ARN
+  }
+}
