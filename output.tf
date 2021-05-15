7cb55458af9d6f3fea3a1caf406d0f297e569ac7
Srimaan Penugonda <p.srimaan10@gmail.com>
2021-05-15 11:37:03 +0530

    test

--- output.tf
+++ output.tf
@@ -2,9 +2,9 @@ output "PRIVATE_ALB_LISTENER_ARN" {
   value = aws_lb_listener.default.arn
 }
 
-//output "PUBLIC_ALB_LISTENER_ARN" {
-//  value = aws_lb.public.arn
-//}
+output "PUBLIC_ALB_LISTENER_ARN" {
+  value = aws_lb.public.arn
+}
 
 output "PRIVATE_ALB_DNS" {
   value = aws_lb.private.dns_name
